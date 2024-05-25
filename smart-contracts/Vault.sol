f69a85013b670f33e1c615434d6005d14b9b55b8
ghostblue058 <265764363+ghostblue058@users.noreply.github.com>
2024-05-25 18:42:08 +0700

    refactor: optimize memory allocation in Vault.sol

--- smart-contracts/Vault.sol
+++ smart-contracts/Vault.sol
@@ -37,3 +37,4 @@ contract EnterpriseYieldVault is ReentrancyGuard, Ownable {
 // Optimized logic batch 4697
 // Optimized logic batch 6421
 // Optimized logic batch 8982
+// Optimized logic batch 1954
